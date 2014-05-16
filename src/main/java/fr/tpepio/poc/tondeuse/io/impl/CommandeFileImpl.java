a47d8f2add9f84fd1788994ec14a65db1aa2230f
tpepio <thomas.pepio@gmail.com>
2014-05-16 15:19:12 +0200

    Refactor du format du fichier de commande. Classe de commande + TU.

--- src/main/java/fr/tpepio/poc/tondeuse/io/impl/CommandeFileImpl.java
+++ src/main/java/fr/tpepio/poc/tondeuse/io/impl/CommandeFileImpl.java
@@ -1,12 +1,12 @@
 package fr.tpepio.poc.tondeuse.io.impl;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -15,7 +15,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import fr.tpepio.poc.tondeuse.core.impl.Mower;
+import fr.tpepio.poc.tondeuse.domain.Case;
 import fr.tpepio.poc.tondeuse.domain.Grid;
+import fr.tpepio.poc.tondeuse.enumeration.EnumCardinalPoint;
 import fr.tpepio.poc.tondeuse.io.ICommandFile;
 
 /**
@@ -33,15 +35,22 @@ public class CommandeFileImpl implements ICommandFile {
 	private static final String IO_EXCEPTION = "Impossible de lire {0}.";
 	
 	/** Séparateur dans le fichier de configuration externe : caractère espace. */
-	private static final String SEPARATOR = " ";
+	private static final String SEPARATOR = ":";
 	
-	private static final String PREFIX_INIT_GRID = "INIT_GRID";
-
+	/** Préfixe des lignes qui permettent d'initialiser une grille. */
+	private static final String PREFIX_INIT_GRID = "GRID";
+	
+	/** Préfixe des lignes qui permettent d'initialiser une tondeuse. */
+	private static final String PREFIX_INIT_MOWER = "MOWER";
+	
+	/** Message d'erreur utilisé lorsqu'on ne parvient pas à trouver le fichier. */
+	private static final String FILE_NOT_FOUND_EXCEPTION = "Le fichier {0} n'existe pas.";
+	
+	/** Message d'erreur utilisé lorsqu'on est incapable de lire au moins une ligne concernant une tondeuse. */
+	private static final String IO_EXCEPTION_INIT_MOWERS = "Erreur lors de l'initialisation des tondeuses.";
+	
 	/** Fichier de commandes. */
 	private Path path;
-
-	/** Reader buffurisé. */
-	private BufferedReader bufferedReader;
 
 	/**
 	 * Constructeur.
@@ -61,6 +70,8 @@ public class CommandeFileImpl implements ICommandFile {
 	 */
 	@Override
 	public Grid buildGrid() {
+		Grid grid = null;
+		
 		try (Stream<String> stream = Files.lines(this.path)) {
 			Optional<String> initGridCommand = stream.filter(line -> line.startsWith(PREFIX_INIT_GRID)).findFirst();
 			
@@ -68,12 +79,14 @@ public class CommandeFileImpl implements ICommandFile {
 				String[] command = initGridCommand.get().split(SEPARATOR);
 				String abscisse  = command[1];
 				String ordonnee = command[2];
+				
+				grid = new Grid(Integer.valueOf(abscisse), Integer.valueOf(ordonnee));
 			}
 		} catch (IOException e) {
 			LOGGER.error(MessageFormat.format(IO_EXCEPTION, this.path.getFileName().toString()));
 		}
 		
-		return null;
+		return grid;
 	}
 
 	/**
@@ -81,7 +94,31 @@ public class CommandeFileImpl implements ICommandFile {
 	 */
 	@Override
 	public List<Mower> buildMowers() {
-		return null;
+		List<Mower> result = new ArrayList<Mower>();
+
+		try (Stream<String> stream = Files.lines(this.path)) {
+			stream.filter(line -> line.startsWith(PREFIX_INIT_MOWER)).forEach(line -> result.add(this.getMowerFromLine(line.split(SEPARATOR))));
+		} catch (IOException e) {
+			LOGGER.error(MessageFormat.format(IO_EXCEPTION, this.path.getFileName().toString()));
+		}
+		
+		return result;
 	}
 
+	/**
+	 * Extrait une tondeuse à partir d'une ligne.
+	 * 
+	 * @param line la ligne sous forme de tableau de String.
+	 * @return la tondeuse.
+	 */
+	private Mower getMowerFromLine(String[] line) {
+		Case aCase = new Case(Integer.valueOf(line[1]), Integer.valueOf(line[2]));
+		
+		Mower mower = new Mower();
+		mower.setCurrentCase(aCase);
+		mower.setOrientation(EnumCardinalPoint.fromCode(line[3]));
+		
+		return mower;
+	}
+	
 }
---
+++ src/test/java/fr/tpepio/poc/tondeuse/io/CommandeFileImplTest.java
@@ -0,0 +1,47 @@
+package fr.tpepio.poc.tondeuse.io;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import fr.tpepio.poc.tondeuse.core.impl.Mower;
+import fr.tpepio.poc.tondeuse.domain.Grid;
+import fr.tpepio.poc.tondeuse.enumeration.EnumCardinalPoint;
+import fr.tpepio.poc.tondeuse.io.impl.CommandeFileImpl;
+
+public class CommandeFileImplTest {
+
+	private CommandeFileImpl commande = new CommandeFileImpl("./src/test/resources/sample-test-file.txt");
+	
+	@Test
+	public void testBuildGrid() {
+		Grid grid = this.commande.buildGrid();
+		
+		Assert.assertNotNull("La grille ne doit pas être null.", grid);
+		Assert.assertEquals("L'abscisse de la case haut-droit est incorrecte.", Integer.valueOf(2), grid.getAbscisseUpperRight());
+		Assert.assertEquals("L'ordonnée de la case haut-droit est incorrecte.", Integer.valueOf(1), grid.getOrdonneeUpperRight());
+	}
+	
+	@Test
+	public void testBuildMowers() {
+		List<Mower> mowers = this.commande.buildMowers();
+		
+		Assert.assertEquals("La taille de la liste est incorrecte.", 3, mowers.size());
+		
+		Mower mower = mowers.get(0);
+		Assert.assertEquals("Abscisse incorrecte.", Integer.valueOf(1), mower.getCurrentCase().getAbscisse());
+		Assert.assertEquals("Ordonnée incorrecte.", Integer.valueOf(2), mower.getCurrentCase().getOrdonnee());
+		Assert.assertEquals("Orientation incorrecte", EnumCardinalPoint.NORTH, mower.getOrientation());
+		
+		mower = mowers.get(1);
+		Assert.assertEquals("Abscisse incorrecte.", Integer.valueOf(3), mower.getCurrentCase().getAbscisse());
+		Assert.assertEquals("Ordonnée incorrecte.", Integer.valueOf(3), mower.getCurrentCase().getOrdonnee());
+		Assert.assertEquals("Orientation incorrecte", EnumCardinalPoint.EAST, mower.getOrientation());
+		
+		mower = mowers.get(2);
+		Assert.assertEquals("Abscisse incorrecte.", Integer.valueOf(5), mower.getCurrentCase().getAbscisse());
+		Assert.assertEquals("Ordonnée incorrecte.", Integer.valueOf(5), mower.getCurrentCase().getOrdonnee());
+		Assert.assertEquals("Orientation incorrecte", EnumCardinalPoint.SOUTH, mower.getOrientation());
+	}
+}
